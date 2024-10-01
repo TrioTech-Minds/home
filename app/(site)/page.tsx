1a6fc3a97e41c3b9225f616a8a33986d4c0c64e7
Muhammad-AliHaider <107212677+Muhammad-AliHaider@users.noreply.github.com>
2024-10-01 22:50:26 +0500

    website added

---
+++ app/(site)/page.tsx
@@ -0,0 +1,36 @@
+import About from "@/components/About";
+import Blog from "@/components/Blog";
+import Brands from "@/components/Brands";
+import CTA from "@/components/CTA";
+import Contact from "@/components/Contact";
+import FAQ from "@/components/FAQ";
+import Feature from "@/components/Features";
+import FeaturesTab from "@/components/FeaturesTab";
+import FunFact from "@/components/FunFact";
+import Hero from "@/components/Hero";
+import Integration from "@/components/Integration";
+import { Metadata } from "next";
+import Pricing from "@/components/Pricing";
+import Testimonial from "@/components/Testimonial";
+
+export const metadata: Metadata = {
+  title: "Next.js Starter Template for SaaS Startups - Solid SaaS Boilerplate",
+  description: "This is Home for Solid Pro",
+  // other metadata
+};
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <Brands />
+      <Feature />
+      <About />
+      <FeaturesTab />
+      <FunFact />
+      <Testimonial />
+      <Contact />
+      <Blog />
+    </main>
+  );
+}
